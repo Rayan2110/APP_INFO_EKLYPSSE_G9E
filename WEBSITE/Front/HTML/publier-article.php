68029e6d2c65190c49b262f6639f8f10dc10b72c
Xyno-coder <gauthier.schmitt1@gmail.com>
2024-04-19 09:08:48 +0200

    Ajout lien  + Page article

--- WEBSITE/Front/HTML/publier-article.php
+++ WEBSITE/Front/HTML/publier-article.php
@@ -3,10 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Eklypsse</title>
 </head>
 <body>
     <h1>Admin</h1>
-    <a href="publier-article.php">Publier un nouvel article</a>
 </body>
 </html>
