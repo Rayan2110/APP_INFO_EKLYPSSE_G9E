4e04d42a1b0e28e4b1eb6c53d7f1374b5ac412d6
Mathurinnnnnn <153925254+Mathurinnnnnn@users.noreply.github.com>
2024-05-01 01:48:41 +0200

    modif admin + delete faq

--- WEBSITE/Front/HTML/publier-article.php
+++ WEBSITE/Front/HTML/publier-article.php
@@ -1,19 +1,17 @@
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../CSS/admin.css">
+    <link href="https://cdn.jsdelivr.net/npm/boxicons@2.1.1/css/boxicons.min.css" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
 <?php
-                // Inclure le fichier header.php
-                include 'header.php';
-                ?>
+// Inclure le fichier header.php
+include 'header.php';
+?>
     
 <a href="admin.php" class="Admin" ><h1>Admin</h1></a>
 <br>
@@ -47,30 +45,37 @@ if (isset($_POST['supprimer'])) {
     // Execute code for deleting FAQ entries
     $bdd = new PDO('mysql:host=localhost;dbname=espace_admins', 'root', '');
 
+    ?>
+    <h2>Liste des F.A.Q</h2> 
+    <div class="liste">
+
+    <?php
     $recupArticle = $bdd->query('SELECT * FROM faq');
     while ($article = $recupArticle->fetch()) {
         ?>
         <div class="faq active">
-            <button class="accordion">
-                <?= $article['question'] ?>
-                <i class="fa-solid fa-chevron-down"></i>
-            </button>
-            <div class="pannel">
-                <p><?= $article['reponse'] ?></p>
-                <!-- Add a hidden input to store the article ID -->
-                <input type="hidden" name="article_id" value="<?= $article['id'] ?>">
-                <!-- Change the button to an input type="submit" -->
-                <a href="delete.php?id=<?= $article['id'] ?>">
-                <button style="color:white; background-color : red; margin-bottom : 10px;" >Supprimer</button></a>
-                
+            <div class= "questRep">
+                <button class="accordion">
+                    <?= $article['question'] ?>
+                    <i class="fa-solid fa-chevron-down"></i>
+                </button>
+                <div class="pannel">
+                    <p><?= $article['reponse'] ?></p>
+                    <!-- Add a hidden input to store the article ID -->
+                    <input type="hidden" name="article_id" value="<?= $article['id'] ?>">
+                </div>
             </div>
+            <!-- Change the button to an input type="submit" -->
+            <a href="delete.php?id=<?= $article['id'] ?>">
+            <button class="btnDelete"><i class='bx bx-trash'></i> Supprimer</button></a>
         </div>
+    
 <?php
     }
 }
-
-
 ?>
+</div>
+
 
 <script>
     var acc = document.getElementsByClassName("accordion");
@@ -80,11 +85,14 @@ if (isset($_POST['supprimer'])) {
         acc[i].addEventListener("click", function() {
             this.classList.toggle("active");
             var panel = this.nextElementSibling;
+            var chevron = this.querySelector('.fa-chevron-down');
 
-            if (panel.style.display === "block") {
+            if (panel.style.display === "flex") {
                 panel.style.display = "none";
+                chevron.classList.toggle("active2");
             } else {
-                panel.style.display = "block";
+                panel.style.display = "flex";
+                chevron.classList.toggle("active2");
             }
         });
     }
