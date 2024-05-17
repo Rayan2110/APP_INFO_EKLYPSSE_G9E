578ad3acddf1d5ee4e6a4bec02d00b44fd0d7832
Eric Tong <erictong46@gmail.com>
2024-05-17 12:29:48 +0200

    ajoute des commentaires à la page Home, ajout d'un option envoie comment en header et gestion des comment pour les admin

--- WEBSITE/Front/HTML/admin.php
+++ WEBSITE/Front/HTML/admin.php
@@ -20,6 +20,7 @@
         <button type="button" name="PublierArticle" class="PublierArticle" ><a href="publier-article.php">Gestion de la FAQ</a></button>
         <button type="button" name="AfficherUser" class="AfficherUser" ><a href="afficher_utilisateur.php">Gestion des utilisateurs</a></button>
         <button type="button" name="ButtonEvent" class="ButtonEvent" ><a href="ButtonEvent.php">Gestion des évènements</a></button>
+        <button type="button" name="GestionCommentaire" class="GestionCommentaire" ><a href="Gestion_Commentaire.php">Gestion des commentaires</a></button>
         <button type="button" name="CGU" class="CGU" ><a href="gestion-cgu.php">Modifier les CGU et mentions légales</a></button>
     </div>
     <br>
